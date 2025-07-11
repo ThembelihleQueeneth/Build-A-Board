58bc90add1ccce381704e09b4be42942da59d614
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-07-11 10:29:07 +0200

    third commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup(popupId) {
+        document.getElementById(popupId).classList.add("open-popup");
+    }
+
+    function closePopup(popupId) {
+        document.getElementById(popupId).classList.remove("open-popup");
+    }
