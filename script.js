8503c27986e9938558aac95a6ac6a84f6d14e165
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2025-07-11 14:47:51 +0200

    fourth commit

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function openPopup(popupId) {
     function closePopup(popupId) {
         document.getElementById(popupId).classList.remove("open-popup");
     }
+ const viewtoggle = document.getElementById("viewToggle");
+ const motherboardView = document.getElementById("div1");
+ const biosView = document.getElementById("div2");
+
+  viewtoggle.addEventListener("click", () => {
+      
+    const motherboardActive = motherboardView.classList.contains("active-view");
+
+    motherboardView.classList.toggle("active-view", !motherboardActive);
+    motherboardView.classList.toggle("hidden-view", motherboardActive);
+    biosView.classList.toggle("active-view", motherboardActive);
+    biosView.classList.toggle("hidden-view", !motherboardActive);
+
+    viewtoggle.textContent = motherboardActive ? "Switch to Motherboard View" : "Switch to BIOS View";
+
+  });
+
+  document.addEventListener("keydown", (event => {
+    if(event.key === "Escape" && biosView.classList.contains('active-view')) {
+        viewtoggle.click();
+        
+    }
+  }))
